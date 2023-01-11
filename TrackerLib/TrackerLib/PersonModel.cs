40ebeead733a3c5738721036fff28744f2daed4c
KairyFischer <122435090+KairyFischer@users.noreply.github.com>
2023-01-11 09:10:42 +0100

    Add files via upload

---
+++ TrackerLib/TrackerLib/PersonModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLib
+{
+    internal class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAdress { get; set; }
+        public int Cellphone { get; set; }
+
+
+
+    }
+}
